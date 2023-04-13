9af05b629a0c89576bcf49b1f5c78dad65909de3
Talhagh <127670171+Talhagh@users.noreply.github.com>
2023-04-14 04:01:34 +0500

    Delete 7.js

--- 7.js
+++
@@ -1,6 +0,0 @@
-function getUniqueStrings(arr) {
-    return [...new Set(arr)];
-  }
-  const myArr = ['Tikka', 'Biryani', 'Karachi Tea', 'Tikka', 'Biryani'];
-const uniqueStrings = getUniqueStrings(myArr);
-console.log(uniqueStrings); // Output: ["Tikka", "Biryani", "Karachi Tea"]
